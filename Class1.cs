c09221f581530830f9cd20003e8d69c8ec1029e7
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 23:47:15 +0530

    ..

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunits
+{
+    public class Class1
+    {
+        public int add(int a , int b)
+        {
+            return a+b;
+        }
+        public int sub(int a , int b)
+        {
+            return a-b;
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunits
+{
+    [TestFixture]
+     class Class2
+    {
+        static void Main(string[] args)
+        {
+            [TestCase]
+             void add()
+            {
+                Console.WriteLine("hello");
+            }
+            [TestCase]
+             void sub()
+            {
+                Console.WriteLine("hello removed");
+            }
+        }
+        
+    }
+}
